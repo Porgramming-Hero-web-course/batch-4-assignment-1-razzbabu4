41acd89916f09c46a8dce3e0ad813941634a9a82
Tareq <razzbabut@gmail.com>
2024-11-07 00:34:06 +0600

    complete problem-1

--- src/problem-1.ts
+++ src/problem-1.ts
@@ -1,14 +1,11 @@
 {
 
+    const sumArray = (arr: number[]): number => {
+        return arr.reduce((acc, num) => acc + num, 0)
+    }
+
+    const sum = sumArray([1, 2, 3])
+    // console.log(sum);
 
 
-
-
-
-
-
-
-
-
-    
 }
