3bcd9604ab434bc6a6caaac5c1bdeede0ff9224e
Tareq <razzbabut@gmail.com>
2024-11-07 06:53:02 +0600

    complete problem-6

---
+++ src/problem-6.ts
@@ -0,0 +1,21 @@
+
+interface Profile {
+    name: string;
+    age: number;
+    email: string;
+}
+
+interface PartialUpdate {
+    name?: string;
+    age?: number;
+    email?: string;
+}
+
+const updateProfile = (profile: Profile, partial: PartialUpdate): Profile => {
+
+    return { ...profile, ...partial }
+
+}
+
+const myProfile = { name: "Alice", age: 25, email: "alice@example.com" };
+updateProfile(myProfile, { name: "Bruce" })
